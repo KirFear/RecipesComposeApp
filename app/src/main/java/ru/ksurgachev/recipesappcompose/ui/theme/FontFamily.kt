38f953fec8e1b127e3d58fba9def6c6b34b4933f
KirFear <sukir@list.ru>
2026-05-06 18:33:41 +0300

    RCA-05 Setup Fonts

---
+++ app/src/main/java/ru/ksurgachev/recipesappcompose/ui/theme/FontFamily.kt
@@ -0,0 +1,15 @@
+package ru.ksurgachev.recipesappcompose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import ru.ksurgachev.recipe_app_compose.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+            Font(R.font.montserrat_regular, FontWeight.Normal),
+)
+
+val montserratAlternatesFontFamily = FontFamily(
+    Font(R.font.montserrat_alternates_semibold, FontWeight.SemiBold)
+)
